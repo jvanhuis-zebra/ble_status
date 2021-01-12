6e9c4816378a098def8812786e0a1a5300951c0f
Rumen Russanov <rumenrussanov@Rumens-MacBook-Pro.local>
2021-01-12 12:42:07 +0200

    update android

--- android/src/main/java/com/example/ble_status/BleStatusPlugin.java
+++ android/src/main/java/com/example/ble_status/BleStatusPlugin.java
@@ -1,6 +1,13 @@
 package com.example.ble_status;
 
+import android.app.Application;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.os.Build;
+
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.MethodCall;
@@ -17,16 +24,23 @@ public class BleStatusPlugin implements FlutterPlugin, MethodCallHandler {
   /// when the Flutter Engine is detached from the Activity
   private MethodChannel channel;
 
+  private BluetoothManager mBluetoothManager;
+  private BluetoothAdapter mBluetoothAdapter;
+
+  @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
   @Override
   public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+    mBluetoothManager = (BluetoothManager) flutterPluginBinding.getApplicationContext().getSystemService(Context.BLUETOOTH_SERVICE);
+    mBluetoothAdapter = mBluetoothManager.getAdapter();
+
     channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "ble_status");
     channel.setMethodCallHandler(this);
   }
 
   @Override
   public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
-    if (call.method.equals("getPlatformVersion")) {
-      result.success("Android " + android.os.Build.VERSION.RELEASE);
+    if (call.method.equals("isOn")) {
+      result.success(mBluetoothAdapter.isEnabled());
     } else {
       result.notImplemented();
     }
